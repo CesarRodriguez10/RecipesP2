2aa4298a59f8bfad40f624121666b7e7ddf21c45
CesarRodriguez10 <alejorz2004@gmail.com>
2026-05-19 21:44:17 -0500

    Codigo completo de GetStatsUseCase

--- app/src/main/java/com/example/recipesp2/domain/usecase/GetStatsUseCase.kt
+++ app/src/main/java/com/example/recipesp2/domain/usecase/GetStatsUseCase.kt
@@ -1,4 +1,9 @@
 package com.example.recipesp2.domain.usecase
 
-class GetStatsUseCase {
+import com.example.recipesp2.data.model.RecipeStats
+import com.example.recipesp2.data.repository.RecipeRepository
+
+class GetStatsUseCase(private val repository: RecipeRepository) {
+    suspend operator fun invoke(recipeId: Int): Result<RecipeStats> =
+        repository.getStats(recipeId)
 }
